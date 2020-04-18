c5115f0f7157d6dbde02f40c83882c40ffb91648
Grant Belcher <gtbelch@gmail.com>
2020-04-18 14:43:43 -0700

    creates route to get all posts

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,12 +1,63 @@
 const express = require('express')
 const router = express.Router()
+const { check, validationResult } = require('express-validator')
+const auth = require('../../middleware/auth')
 
-// Post api/posts
-// Register User
-// Public
+const Profile = require('../../models/Profile')
+const Post = require('../../models/Post')
+const User = require('../../models/User')
+// @route   POST api/posts
+// @desc    CREATE a post
+// @access  PRIVATE
 
-router.post('/', (req, res) => {
-    console.log(req.body)
+router.post('/',
+    [
+        auth,
+        [
+            check('text', 'Text is required')
+                .not()
+                .isEmpty()
+        ]
+        
+    ], async(req, res) => {
+        const errors = validationResult(req)
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() })
+        }
+        try {
+            const user = await User.findById(req.user.id).select('-password'); // -password excludes pass from query result
+            const newPost = new Post({
+                text: req.body.text,
+                name: user.name,
+                avatar: user.avatar,
+                user: req.user.id
+            }
+)
+            const post = await newPost.save()
+
+            res.json(post)
+
+        } catch (err) {
+        // User is logged already logged in => token w/ userId is in req.user.id
+            console.error(err.message);
+            res.status(500).send('Server Error');
+        }
+
+    }
+)
+
+// @route   GET api/posts
+// @desc    get all posts
+// @access  private
+
+router.get('/', auth, async (req, res) => {
+    try {                                  // sort by most recent date
+        const posts = await Post.find().sort({ date: -1 })
+        res.json(posts)
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
 })
 
 module.exports = router
