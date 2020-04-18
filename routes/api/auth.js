b15dc0d2c052be127706445c826583693dbe1401
Grant Belcher <gtbelch@gmail.com>
2020-04-17 18:56:46 -0700

    auth route verifies user with token

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -2,6 +2,8 @@ const express = require('express')
 const router = express.Router();
 const auth = require('../../middleware/auth')
 
+// bring in User model
+const User = require('../../models/User')
 
 // @route GET api/auth
 // @desc Test Route
@@ -9,6 +11,18 @@ const auth = require('../../middleware/auth')
 
 
 // use auth middleware to protect route
-router.get('/', auth, (req, res) => res.send('auth appears here'))
+router.get('/', auth, async (req, res) => {
+    try {
+        // access user from collection,    req.user is defined in middleware function
+
+        const user = await User.findById(req.user.id).select('-password') ;
+                                                        // '-password' => excludes password field from user data
+        // send the user information when user logs in
+        res.json(user);
+    } catch(err) {
+        console.error(err.message)
+        res.status(500).send('Server Error')
+    }
+});
 
 module.exports = router
