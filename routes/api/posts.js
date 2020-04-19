e9ff9efbe7a9b712f65068723eda18a9aba610fa
Grant Belcher <gtbelch@gmail.com>
2020-04-19 11:32:52 -0700

    sets up envm for UI with Sass

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -132,4 +132,130 @@ router.delete('/:id', auth, async (req, res) => {
     }
 })
 
+// @route   PUT api/posts/like/:id
+// @desc    PUT a post
+// @access  private
+
+router.put('/like/:id', auth, async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+
+        // CHECK IF POST HAS ALREADY BEEN LIKED
+        if (post.likes.filter(like => like.user.toString() === req.user.id).length)  {
+            // ^^^ IF LENGTH OF FILTERED ARRAY IS GREATER THAN ZERO => already liked
+            return res.status(400).json({ msg: 'Post already liked' });
+        }
+        // add user to beginning of 'likes' list
+        post.likes.unshift({ user: req.user.id })
+
+        // save updated post document
+        await post.save()
+        //respond with all likes, this will be used on Front END!!!!
+        res.json({liked: post.likes})
+
+    } catch (err) {
+        console.error(err)
+        res.status(500).send('SERVER ERROR')
+    }
+})
+
+// @route   PUT api/posts/like/:id
+// @desc    PUT a post
+// @access  private
+
+router.put('/unlike/:id', auth, async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+
+        // CHECK IF POST HAS ALREADY BEEN LIKED
+        if (post.likes.filter(like => like.user.toString() === req.user.id).length === 0)  {
+            // ^^^ IF LENGTH OF FILTERED ARRAY IS ZERO => has not yet been liked
+            return res.status(400).json({ msg: 'Post has not yet been liked' });
+        }
+        // GET index of like to be removed
+        const removeIndex = post.likes.map(like => like.user.toString()).indexOf(req.user.id);
+
+        post.likes.splice(removeIndex, 1);
+
+        // save updated post document
+        await post.save()
+        //respond with all likes, this will be used on Front END!!!!
+        res.json({liked: post.likes})
+
+    } catch (err) {
+        console.error(err)
+        res.status(500).send('SERVER ERROR')
+    }
+})
+
+// @route   PUT api/posts/comment/:id
+// @desc    add comment to a post
+// @access  private
+
+router.put('/comment/:id', [auth, 
+    [
+        check('text', 'text is required')
+            .not()
+            .isEmpty()
+    ]
+    ], 
+    async (req, res) => {
+        try {
+            const errors = validationResult(req)
+            if (!errors.isEmpty()) {
+                return res.status(400).json({ errors: errors.array() })
+            }
+
+            // find user by id
+            const user = await User.findById(req.user.id).select('-password')
+
+            // find post by id
+            const post = await Post.findById(req.params.id)
+            if (!post) {
+                return res.status(404).json({ msg: 'Post not found' })
+            }
+            
+            const newComment = {
+                text: req.body.text,
+                name: user.name,
+                avatar: user.avatar,
+                user: req.user.id
+            }
+            post.comments.unshift(newComment)
+            await post.save()
+            res.json(post.comments)
+        } catch (err) {
+            console.error(err)
+            res.status(500).json({ msg: 'Server Error' })
+        }
+    })
+
+// @route   DELETE api/posts/comment/:id/:comment_id
+// @desc    Delete a comment on a post
+// @access  private
+
+router.delete('/comment/:id/:comment_id', auth, async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id)
+        const commentIndex = post.comments.map(comment => comment.id).indexOf(req.params.comment_id)
+        const comment = (post.comments[commentIndex])
+    
+        if (!comment) {
+            return res.status(404).json({msg: 'comment not found'})
+        }
+
+
+        if (comment.user.toString() !== req.user.id) {
+            return res.status(401).json({msg: "Bad request"})
+        }
+        post.comments.splice(commentIndex, 1)
+        await post.save()
+        res.json(post.comments)
+
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({msg: 'Server Error'})
+    }
+})
+
 module.exports = router
